171e870fd03ffd96b6b90b2d5611e515d85f3788
JW Moon <moon910904@gmail.com>
2025-11-30 19:52:29 +0900

    feature: add parseToEmptyRuby function for kanji text formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function parseToEmptyRuby(input: string): string {
+  const kanjiRegex = /([\u4E00-\u9FFF]+)/g;
+  return input.replace(kanjiRegex, (match) => {
+    return `<ruby>${match}<rp>(</rp><rt>???</rt><rp>)</rp></ruby>`;
+  });
+}
